a64bfb60a8841822d495a50d9a15a95a14a2d435
devcxl <64475363+devcxl@users.noreply.github.com>
2023-08-04 23:42:50 +0800

    update: build script

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 pyinstaller --clean wechatbot.spec
 cp config.json ./dist/
-docker build -t wechatbot .
+docker build -t registry.cn-shanghai.aliyuncs.com/devcxl/wechatbot .
 docker rm -f chatbot
-docker run --network host --name chatbot -d wechatbot:latest
+docker run --network host --name chatbot -d registry.cn-shanghai.aliyuncs.com/devcxl/wechatbot:latest
 docker system prune -f
+docker push registry.cn-shanghai.aliyuncs.com/devcxl/wechatbot:latest
