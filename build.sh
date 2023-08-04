bca33bb9bdf7bc657b9ef435f211684c3384ecab
devcxl <64475363+devcxl@users.noreply.github.com>
2023-08-04 23:23:33 +0800

    update: build script

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
-# docker build -t wechatbot .
-# docker run  --network host -it wechatbot:latest
-# docker save -o wechatbot.tar wechatbot:latest
-
-docker run -it -v $PWD/:/build/ python:3.6.15 bash
-
-# pip install pypng PyQRCode requests pyinstaller -i https://pypi.tuna.tsinghua.edu.cn/simple/ && pip install --upgrade revChatGPT -i https://pypi.tuna.tsinghua.edu.cn/simple/ && pip install itchat_uos-1.5.0.dev0-py3-none-any.whl
-# pyinstaller -F -c --name wechatbot main.py
-# pyinstaller -F -c --name wechatbot --collect-all main main.py
-# pyinstaller -d all -F main.py
+#!/bin/bash
+pyinstaller --clean wechatbot.spec
+cp config.json ./dist/
+docker build -t wechatbot .
+docker rm -f chatbot
+docker run --network host --name chatbot -d wechatbot:latest
+docker system prune -f
