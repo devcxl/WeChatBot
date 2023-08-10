4cad77c31a47231eb4d0cb83edec2fd0cb1be3f4
devcxl <orange.cxl@qq.com>
2023-08-11 02:57:33 +0800

    format: all code

--- command/base_command.py
+++ command/base_command.py
@@ -1,5 +1,6 @@
 from abc import ABC
 
+
 class BaseCommand(ABC):
     def __init__(self) -> None:
         pass
@@ -9,6 +10,3 @@ class BaseCommand(ABC):
 
     def execute(self, user=None, params=None, isGroup=False) -> str:
         raise NotImplementedError
-
-
-
