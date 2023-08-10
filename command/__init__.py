c620a97a2fd1de260f86d5f764d3110f64bbe379
devcxl <orange.cxl@qq.com>
2023-08-10 21:08:01 +0800

    update: EmojiCommand

--- command/__init__.py
+++ command/__init__.py
@@ -125,7 +125,7 @@ class WeatherCommand(BaseCommand):
             return "不支持的地区"
 
 
-class RandomPictrueCommand(BaseCommand):
+class EmojiCommand(BaseCommand):
 
     def __init__(self) -> None:
         self.upload_dir='/tmp/'
@@ -135,26 +135,51 @@ class RandomPictrueCommand(BaseCommand):
         return '/emoji'
 
     def execute(self, user=None, params=None, isGroup=False) -> str:
-        import requests,json,itchat
+        
+        if len(params)<=1:
+            return self.random_emoji() 
+        else:
+            if params[1] =='install' and params[2] =='bilibili':
+                self.download_bilibili_emoji()
+                return 'install bilibili emoji successful!'
+    
+    def random_emoji(self):
+        import requests,json
         resp = requests.get(self.api)
         if resp.status_code == 200:
             respBody = json.loads(resp.content);
             emojiResp = requests.get(respBody['text'])
             urlArray = respBody['text'].split('/')
             fileName = urlArray[len(urlArray)-1]
-
             if emojiResp.status_code == 200:
                 with open(self.upload_dir + fileName,'wb') as f:
                     f.write(emojiResp.content)
         return f'@img@{self.upload_dir + fileName}'
 
+    def download_bilibili_emoji(self):
+        import requests
+        # bilibili表情api（免费表情）
+        url = 'http://api.bilibili.com/x/emote/user/panel/web?business=reply'
+        # 请求头
+        headers = {
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36 Edg/99.0.1150.52"}
+        # 获取json数据
+        res = requests.get(url, headers=headers)
+        info = res.json()
+        # #遍历取出表情url，不需要颜文字
+        for package in info['data']['packages'][:3]:
+            for emo in package['emote']:
+                emo['text'], emo['url']
+                jpg = requests.get(emo['url'], headers=headers)
+                # time.sleep(random.random())
+                with open(f'./emoji/{emo["text"]}.png', 'wb') as file:
+                    file.write(jpg.content)
 
 factory = CommandFactory()
 factory.registerCommand(NoticeCommand())
 factory.registerCommand(WeatherCommand())
 factory.registerCommand(GroupCommand())
-factory.registerCommand(RandomPictrueCommand())
-
+factory.registerCommand(EmojiCommand())
 
 # test code
 # if __name__ == "__main__":
