8bc99311517ba3bc05d78e1c2d9254f9b8185786
devcxl <64475363+devcxl@users.noreply.github.com>
2023-08-14 19:48:39 +0800

    update: registerCommand WeiboCommand

--- command/__init__.py
+++ command/__init__.py
@@ -2,8 +2,10 @@ from .command_factory import CommandFactory
 from .emoji_command import EmojiCommand
 from .group_command import GroupCommand
 from .weather_command import WeatherCommand
+from .weibo_command import WeiboCommand
 
 factory = CommandFactory()
 factory.registerCommand(WeatherCommand())
 factory.registerCommand(GroupCommand())
 factory.registerCommand(EmojiCommand())
+factory.registerCommand(WeiboCommand())
