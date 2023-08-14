57501b3d154ea36451068968a277434116cee26a
devcxl <orange.cxl@qq.com>
2023-08-14 23:32:32 +0800

    add: tts command powerd by azure

--- command/__init__.py
+++ command/__init__.py
@@ -3,9 +3,11 @@ from .emoji_command import EmojiCommand
 from .group_command import GroupCommand
 from .weather_command import WeatherCommand
 from .weibo_command import WeiboCommand
+from .tts_command import TTSCommand
 
 factory = CommandFactory()
 factory.registerCommand(WeatherCommand())
 factory.registerCommand(GroupCommand())
 factory.registerCommand(EmojiCommand())
 factory.registerCommand(WeiboCommand())
+factory.registerCommand(TTSCommand())
