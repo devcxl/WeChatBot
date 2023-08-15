f9320bd9e4f8ed319315b89b4c727906dc5948f5
devcxl <64475363+devcxl@users.noreply.github.com>
2023-08-15 18:06:17 +0800

    add: common logger

--- common/__init__.py
+++ common/__init__.py
@@ -1,3 +1,6 @@
+import logging
+
+
 class Struct(object):
     def __init__(self, data):
         for name, value in data.items():
@@ -8,3 +11,30 @@ class Struct(object):
             return type(value)([self._wrap(v) for v in value])
         else:
             return Struct(value) if isinstance(value, dict) else value
+
+
+class Logger:
+    def __init__(self, name, log_file=None, level=logging.INFO) -> None:
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(level)
+        self.handlers = [logging.StreamHandler()]
+        if log_file is not None:
+            self.handlers.append(logging.FileHandler(log_file))
+        # 配置日志基本设置
+        logging.basicConfig(
+            level=level,  # 设置日志级别
+            format='%(asctime)s - %(name)s [%(levelname)s] %(message)s',
+            handlers=self.handlers
+        )
+
+    def info(self, message):
+        self.logger.info(message)
+
+    def warning(self, message):
+        self.logger.warning(message)
+
+    def warn(self, message):
+        self.logger.warning(message)
+
+    def error(self, message):
+        self.logger.error(message)
