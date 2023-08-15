8578084922fbf2cfbb8241f0c8d96079ba24a207
devcxl <64475363+devcxl@users.noreply.github.com>
2023-08-15 18:01:04 +0800

    fix: base_command.py

--- command/base_command.py
+++ command/base_command.py
@@ -1,11 +1,15 @@
 from abc import ABC
+from common import Logger
+
+log = Logger('wechatbot')
 
 
 class BaseCommand(ABC):
     def __init__(self) -> None:
+        log.info(f'命令:{self.getCommandName()}加载成功！')
         pass
 
-    def getCommandName() -> str:
+    def getCommandName(self) -> str:
         raise NotImplementedError
 
     def execute(self, user=None, params=None, isGroup=False) -> str:
