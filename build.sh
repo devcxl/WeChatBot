c95f11cfa52e0738b1bca65a5b880bc80d6ccdfd
devcxl <orange.cxl@qq.com>
2023-08-15 20:20:29 +0800

    fix: build.sh & main logger

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ cp config.json ./dist/
 docker build -t registry.cn-shanghai.aliyuncs.com/devcxl/wechatbot .
 docker rm -f chatbot
 docker run --network host --name chatbot -d registry.cn-shanghai.aliyuncs.com/devcxl/wechatbot:latest
+docker logs --tail 1000 -f chatbot
 docker system prune -f
 docker push registry.cn-shanghai.aliyuncs.com/devcxl/wechatbot:latest
