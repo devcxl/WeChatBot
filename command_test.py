bbb7253e23073feb2fc506d6c2fc89826ebed1aa
devcxl <64475363+devcxl@users.noreply.github.com>
2023-09-06 00:35:20 +0800

    format common_test

--- command_test.py
+++ command_test.py
@@ -2,6 +2,7 @@ from command import factory
 
 user = {}
 
+
 def emoji_test():
     executor = factory.getCommand('/emoji')
     print(executor.execute(user, ['/emoji']))
@@ -15,14 +16,15 @@ def tts_test():
     tts = factory.getCommand('/tts')
     tts.execute(user, ['/tts', '我要给你完整的童年。'])
 
+
 def weibo_test():
-    weibo_hot=factory.getCommand('/weibo')
-    weibo_hot.execute(user,['/weibo'])
+    weibo_hot = factory.getCommand('/weibo')
+    weibo_hot.execute(user, ['/weibo'])
+
 
 def weather_test():
     weather = factory.getCommand('/weather')
-    weather.execute(user,['/weather','北京'])
-
+    weather.execute(user, ['/weather', '北京'])
 
 
 if __name__ == "__main__":
