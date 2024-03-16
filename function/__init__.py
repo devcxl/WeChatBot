de976f61000bcd5a5323893f7fc746ea1fa37e37
devcxl <64475363+devcxl@users.noreply.github.com>
2024-03-17 06:21:19 +0800

    add: weather function call

--- function/__init__.py
+++ function/__init__.py
@@ -1,9 +1,9 @@
 from function.factory import Functions
+from function.weather_function import WeatherFunction
 
 functions = Functions()
 
-# functions.register(EmailFunction())
-# functions.register(WeatherFunction())
+functions.register(WeatherFunction)
 
 function_declares = functions.get_all_declare()
 
--- function/factory.py
+++ function/factory.py
@@ -1,4 +1,13 @@
+import logging
+
 from function.base import BaseFunction
+
+log = logging.getLogger('functions factory')
+
+
+class FunctionRegisterError(RuntimeError):
+    def __init__(self, msg):
+        self.msg = msg
 
 
 class Functions:
@@ -9,8 +18,11 @@ class Functions:
         self.declares = []
         self.available = {}
 
-    def register(self, function: BaseFunction) -> None:
-        self.functions.append(function)
+    def register(self, function) -> None:
+        try:
+            self.functions.append(function())
+        except FunctionRegisterError as e:
+            log.warning(e)
 
     def all(self) -> list[BaseFunction]:
         return self.functions
