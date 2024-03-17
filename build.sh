f52bff297875f2d7b6f74fb0a8bca4d6a62f192d
devcxl <64475363+devcxl@users.noreply.github.com>
2024-03-18 05:05:20 +0800

    remove deprecated build script

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-pyinstaller --clean wechatbot.spec
-docker build -t registry.cn-shanghai.aliyuncs.com/devcxl/wechatbot:dev .
-docker system prune -f
-docker push registry.cn-shanghai.aliyuncs.com/devcxl/wechatbot:dev
