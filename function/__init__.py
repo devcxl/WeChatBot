6c764c5b4c0bb1f6452792ca078627155ef97db3
devcxl <64475363+devcxl@users.noreply.github.com>
2024-03-20 14:21:02 +0800

    register web search function call

--- function/__init__.py
+++ function/__init__.py
@@ -2,12 +2,14 @@ from function.current_time_function import CurrentTimeFunction
 from function.email_send_function import EmailFunction
 from function.factory import Functions
 from function.weather_function import WeatherFunction
+from function.web_search_function import WebSearchFunction
 
 functions = Functions()
 
 functions.register(WeatherFunction)
 functions.register(CurrentTimeFunction)
 functions.register(EmailFunction)
+functions.register(WebSearchFunction)
 
 function_declares = functions.get_all_declare()
 
