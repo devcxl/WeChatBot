e72a512ca7057b4bada7d5e37106926b25626fe8
devcxl <64475363+devcxl@users.noreply.github.com>
2024-04-04 20:33:31 +0800

    Revert "update: test_connect url"
    This reverts commit 4d2bf0869e2a81bcff94790e6364c21f2b9f2ff2.

--- itchat/utils.py
+++ itchat/utils.py
@@ -1,9 +1,4 @@
-import copy
-import hashlib
-import logging
-import re
-import sys
-import traceback
+import re, os, sys, subprocess, copy, traceback, logging, hashlib
 
 try:
     from HTMLParser import HTMLParser
@@ -11,7 +6,6 @@ except ImportError:
     from html.parser import HTMLParser
 try:
     from urllib import quote as _quote
-
     quote = lambda n: _quote(n.encode('utf8', 'replace'))
 except ImportError:
     from urllib.parse import quote
@@ -26,7 +20,6 @@ emojiRegex = re.compile(r'<span class="emoji emoji(.{1,10})"></span>')
 htmlParser = HTMLParser()
 if not hasattr(htmlParser, 'unescape'):
     import html
-
     htmlParser.unescape = html.unescape
     # FIX Python 3.9 HTMLParser.unescape is removed. See https://docs.python.org/3.9/whatsnew/3.9.html
 try:
@@ -39,55 +32,47 @@ else:
     BLOCK = b
 friendInfoTemplate = {}
 for k in ('UserName', 'City', 'DisplayName', 'PYQuanPin', 'RemarkPYInitial', 'Province',
-          'KeyWord', 'RemarkName', 'PYInitial', 'EncryChatRoomId', 'Alias', 'Signature',
-          'NickName', 'RemarkPYQuanPin', 'HeadImgUrl'):
+        'KeyWord', 'RemarkName', 'PYInitial', 'EncryChatRoomId', 'Alias', 'Signature', 
+        'NickName', 'RemarkPYQuanPin', 'HeadImgUrl'):
     friendInfoTemplate[k] = ''
 for k in ('UniFriend', 'Sex', 'AppAccountFlag', 'VerifyFlag', 'ChatRoomId', 'HideInputBarFlag',
-          'AttrStatus', 'SnsFlag', 'MemberCount', 'OwnerUin', 'ContactFlag', 'Uin',
-          'StarFriend', 'Statues'):
+        'AttrStatus', 'SnsFlag', 'MemberCount', 'OwnerUin', 'ContactFlag', 'Uin',
+        'StarFriend', 'Statues'):
     friendInfoTemplate[k] = 0
 friendInfoTemplate['MemberList'] = []
-
 
 def emoji_formatter(d, k):
     ''' _emoji_deebugger is for bugs about emoji match caused by wechat backstage
     like :face with tears of joy: will be replaced with :cat face with tears of joy:
     '''
-
     def _emoji_debugger(d, k):
         s = d[k].replace('<span class="emoji emoji1f450"></span',
-                         '<span class="emoji emoji1f450"></span>')  # fix missing bug
-
+            '<span class="emoji emoji1f450"></span>') # fix missing bug
         def __fix_miss_match(m):
             return '<span class="emoji emoji%s"></span>' % ({
-                                                                '1f63c': '1f601', '1f639': '1f602', '1f63a': '1f603',
-                                                                '1f4ab': '1f616', '1f64d': '1f614', '1f63b': '1f60d',
-                                                                '1f63d': '1f618', '1f64e': '1f621', '1f63f': '1f622',
-                                                            }.get(m.group(1), m.group(1)))
-
+                '1f63c': '1f601', '1f639': '1f602', '1f63a': '1f603',
+                '1f4ab': '1f616', '1f64d': '1f614', '1f63b': '1f60d',
+                '1f63d': '1f618', '1f64e': '1f621', '1f63f': '1f622',
+                }.get(m.group(1), m.group(1)))
         return emojiRegex.sub(__fix_miss_match, s)
-
     def _emoji_formatter(m):
         s = m.group(1)
         if len(s) == 6:
-            return ('\\U%s\\U%s' % (s[:2].rjust(8, '0'), s[2:].rjust(8, '0'))
-                    ).encode('utf8').decode('unicode-escape', 'replace')
+            return ('\\U%s\\U%s'%(s[:2].rjust(8, '0'), s[2:].rjust(8, '0'))
+                ).encode('utf8').decode('unicode-escape', 'replace')
         elif len(s) == 10:
-            return ('\\U%s\\U%s' % (s[:5].rjust(8, '0'), s[5:].rjust(8, '0'))
-                    ).encode('utf8').decode('unicode-escape', 'replace')
+            return ('\\U%s\\U%s'%(s[:5].rjust(8, '0'), s[5:].rjust(8, '0'))
+                ).encode('utf8').decode('unicode-escape', 'replace')
         else:
-            return ('\\U%s' % m.group(1).rjust(8, '0')
-                    ).encode('utf8').decode('unicode-escape', 'replace')
-
+            return ('\\U%s'%m.group(1).rjust(8, '0')
+                ).encode('utf8').decode('unicode-escape', 'replace')
     d[k] = _emoji_debugger(d, k)
     d[k] = emojiRegex.sub(_emoji_formatter, d[k])
-
 
 def msg_formatter(d, k):
     emoji_formatter(d, k)
     d[k] = d[k].replace('<br/>', '\n')
     d[k] = htmlParser.unescape(d[k])
-
 
 def check_file(fileDir):
     try:
@@ -97,12 +82,10 @@ def check_file(fileDir):
     except:
         return False
 
-
 def struct_friend_info(knownInfo):
     member = copy.deepcopy(friendInfoTemplate)
     for k, v in copy.deepcopy(knownInfo).items(): member[k] = v
     return member
-
 
 def search_dict_list(l, key, value):
     ''' Search a list of dict
@@ -111,33 +94,29 @@ def search_dict_list(l, key, value):
         if i.get(key) == value:
             return i
 
-
-def print_line(msg, oneLine=False):
+def print_line(msg, oneLine = False):
     if oneLine:
-        sys.stdout.write(' ' * 40 + '\r')
+        sys.stdout.write(' '*40 + '\r')
         sys.stdout.flush()
     else:
         sys.stdout.write('\n')
     sys.stdout.write(msg.encode(sys.stdin.encoding or 'utf8', 'replace'
-                                ).decode(sys.stdin.encoding or 'utf8', 'replace'))
+        ).decode(sys.stdin.encoding or 'utf8', 'replace'))
     sys.stdout.flush()
-
 
 def test_connect(retryTime=5):
     for i in range(retryTime):
         try:
-            r = requests.get(url="https://wx.qq.com")
+            r = requests.get(config.BASE_URL)
             return True
         except:
             if i == retryTime - 1:
                 logger.error(traceback.format_exc())
                 return False
 
-
 def contact_deep_copy(core, contact):
     with core.storageClass.updateLock:
         return copy.deepcopy(contact)
-
 
 def get_image_postfix(data):
     data = data[:20]
@@ -149,17 +128,15 @@ def get_image_postfix(data):
         return 'jpg'
     return ''
 
-
 def update_info_dict(oldInfoDict, newInfoDict):
     ''' only normal values will be updated here
         because newInfoDict is normal dict, so it's not necessary to consider templates
     '''
     for k, v in newInfoDict.items():
         if any((isinstance(v, t) for t in (tuple, list, dict))):
-            pass  # these values will be updated somewhere else
+            pass # these values will be updated somewhere else
         elif oldInfoDict.get(k) is None or v not in (None, '', '0', 0):
             oldInfoDict[k] = v
-
 
 def calculate_md5(text):
     """Calculate the MD5 hash of a text."""
